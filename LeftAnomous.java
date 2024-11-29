e850af0bab70071ebac1d4c7b5c5154424c0837b
Enrj-Drunk <99850420+Imaginit22@users.noreply.github.com>
2024-11-28 21:38:24 -0500

    fdvdf
    df

--- LeftAnomous.java
+++ LeftAnomous.java
@@ -1,14 +1,17 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.CRServo;
 import java.lang.Math; 
 
 @Autonomous(name="LAuto", group="Autonomous")
-public class LeftAnomous extends LinearOpMode {
+public class LeftAutonomous extends LinearOpMode {
 
     // Hardware used
     private DcMotor leftDrive = null;
@@ -19,132 +22,169 @@ public class LeftAnomous extends LinearOpMode {
 
     private ElapsedTime runtime = new ElapsedTime();
 
+
+    //Map taken from Will's MecanumTeleOp
+    // Declare our motors
+    // Make sure your ID's match your configuration
+    DcMotor frontLeft, backLeft, frontRight, backRight;
+    DcMotor horz;
+    //Servo claw_horz, claw_vertical, claw_horz_rot;
+    CRServo peck;
+    double rot_inc = 0;
+    DcMotor vert1, vert2; 
+    //Where we think we are
+    /*
+     * All of the following assumes:
+     * 1. Looking at the field from the red wall.
+     * 2. Origin is the center of the field.
+     * Robot is positioned as if it were its center. 
+     * If it were centered on the origin, it would be at 0, 0
+     * X axis runs from -72 to 72 as you go from left to right
+     * Y axis runs from -72 to 72 as you go further away from red wall.
+     * (Both of the above as per https://ftc-docs.firstinspires.org/en/latest/game_specific_resources/field_coordinate_system/field-coordinate-system.html)
+     * Facing is with 0 as pointing straight down Y axis, 360 degrees points you straight down again
+     * 
+     */
+    double xPos = 0;
+    double yPos = 0;
+    //rotation as described above
+    double facing = 0;
+
+    // Set target position. NEEDS TO BE CALIBRATED
+    double ticksPerInch = 45.0; 
     @Override
     public void runOpMode() {
-        
-        //Map taken from Will's MecanumTeleOp
-        // Declare our motors
-        // Make sure your ID's match your configuration
-        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
-        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
-        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
-        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
-        // Reverse the right side motors. This may be wrong for your setup.
-        // If your robot moves backwards when commanded to go forwards,
-        // reverse the left side instead.
-        // See the note about this earlier on this page.
-        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        DcMotor horz = hardwareMap.dcMotor.get("horizontal extension1");
-        Servo peck = hardwareMap.servo.get("pecking");
-        Servo claw_horz = hardwareMap.servo.get("claw horizontal");
-        Servo claw_vertical = hardwareMap.servo.get("claw vertical");
-        Servo claw_horz_rot = hardwareMap.servo.get("rotation");
-        double rot_inc = 0;
-        DcMotor vert1 = hardwareMap.dcMotor.get("vertical extension1");
-        DcMotor vert2 = hardwareMap.dcMotor.get("vertical extension2");
-        DcMotor armMotor = hardwareMap.dcMotor.get("armMotor");
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        backLeft = hardwareMap.dcMotor.get("backLeft");
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        backRight = hardwareMap.dcMotor.get("backRight");
+        horz = hardwareMap.dcMotor.get("Horizontal");
+        peck = hardwareMap.crservo.get("peck");
+        //claw_horz = hardwareMap.servo.get("sample claw");
+        //claw_vertical = hardwareMap.servo.get("specimen claw");
+        //claw_horz_rot = hardwareMap.servo.get("rotation");
+        vert1 = hardwareMap.dcMotor.get("Vertical 1");
+        vert2 = hardwareMap.dcMotor.get("Vertical 2");
+        armMotor = hardwareMap.dcMotor.get("armMotor");
         horz.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        horz.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         vert1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        vert1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         vert2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        vert2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        
-
-
         // Wait for start
         waitForStart();
-
-        //Where we think we are
-        /*
-         * All of the following assumes:
-         * 1. Looking at the field from the red wall.
-         * 2. Origin is the center of the field.
-         * Robot is positioned as if it were its center. 
-         * If it were centered on the origin, it would be at 0, 0
-         * X axis runs from -72 to 72 as you go from left to right
-         * Y axis runs from -72 to 72 as you go further away from red wall.
-         * (Both of the above as per https://ftc-docs.firstinspires.org/en/latest/game_specific_resources/field_coordinate_system/field-coordinate-system.html)
-         * Facing is with 0 as pointing straight down Y axis, 360 degrees points you straight down again
-         * 
-         */
-        double xPos = 0;
-        double yPos = 0;
-        //rotation as described above
-        double facing = 0;
-
-        // Set target position. NEEDS TO BE CALIBRATED
-        int ticksPerInch = 1000; 
         
-        private void toPos(double xDest, double yDest, double dDest) {
-            // Reset encoders
-            frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-            // Set to use encoders
-            frontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            frontRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            backLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            backRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            double x = xDest - xPos;
-            double y = yDest - yPos;
-
-            //find distance
-            double hyp = Math.hypot(x, y);
-            
-            //x is opposite, y is adjacent
-            double angle = Math.asin(x/hyp);
-            double radian = Math.toRadians(angle);
-            double PIover4 = Math.PI/4
-            double FRBL = Math.sin(angle - PIover4)
-            double FLBR = Math.sin(angle + PIover4)
-
-
-            frontLeftMotor.setTargetPosition(ticksPerInch * FLBR);
-            frontRightMotor.setTargetPosition(ticksPerInch * FRBL);
-            backLeftMotor.setTargetPosition(ticksPerInch * FRBL);
-            backRightMotor.setTargetPosition(ticksPerInch * FLBR);
-            
-            //if true, normalize by FRBL, else normalize by FLBR
-            double normalize = FLBR
-            if (FLBR > FRBL) {
-                normalize = FRBL
-            }
-            frontLeftMotor.setPower(FLBR / normalize);
-            frontRightMotor.setPower(FRBL / normalize);
-            backLeftMotor.setPower(FRBL / normalize);
-            backRightMotor.setPower(FLBR / normalize);
-
-            while (frontLeftMotor.isBusy() || frontRightMotor.isBusy(), backLeftMotor.isBusy(), backRightMotor.isBusy()) {
-                telemetry.addData("Motors", 
-                frontLeft.getCurrentPosition() + " " +
-                frontRight.getCurrentPosition() + " " +
-                backLeft.getCurrentPosition() + " " +
-                backRight.getCurrentPosition());
-            }
-            //Stop moving and be at new spot
-            xPos = xDest;
-            yPos = yDest;
-            leftMotor.setPower(0);
-            rightMotor.setPower(0);
-        }
+        
         /*
         * Move to position x, y and facing d. 
         */
 
         //camera pseudocode for if i get that 
-        private void toPosCamera(int x, int y, int d) {
+        /*private void toPosCamera(int x, int y, int d) {
 
-        }
+        }*/
         if (opModeIsActive()) {
             runtime.reset();
-
+            toPos(0,100, 0);
+            //toPos(0,0,0);
         }
     }
+    public void toPos(double xDest, double yDest, double dDest) {
+        // Reset encoders
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        backLeft = hardwareMap.dcMotor.get("backLeft");
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        backRight = hardwareMap.dcMotor.get("backRight");
+        horz = hardwareMap.dcMotor.get("Horizontal");
+        peck = hardwareMap.crservo.get("peck");
+        //claw_horz = hardwareMap.servo.get("sample claw");
+        //claw_vertical = hardwareMap.servo.get("specimen claw");
+        //claw_horz_rot = hardwareMap.servo.get("rotation");
+        vert1 = hardwareMap.dcMotor.get("Vertical 1");
+        vert2 = hardwareMap.dcMotor.get("Vertical 2");
+        armMotor = hardwareMap.dcMotor.get("armMotor");
+        horz.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        vert1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        vert2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+        // Set to use encoders
+        
+
+        double x = xDest - xPos;
+        double y = yDest - yPos;
+        telemetry.addData("XandY X: ", String.valueOf(x) + " Y: " + String.valueOf(y));
+        //find distance
+        double hyp = Math.hypot(x, y);
+        telemetry.addData("HYPOT: ", String.valueOf(hyp));
+        //x is opposite, y is adjacent
+        double angle = Math.asin(x/hyp);
+        double radian = Math.toRadians(angle);
+        double PIover4 = Math.PI/4;
+        double FRBL = Math.sin(angle - PIover4);
+        double FLBR = Math.sin(angle + PIover4);
+        telemetry.addData("PRELIMVECT: ", String.valueOf(FRBL) + "  " + String.valueOf(FLBR));
+
+        
+        
+        //if true, normalize by FRBL, else normalize by FLBR
+        double normalize = FLBR;
+        if (FLBR > FRBL) {
+            normalize = FRBL;
+        }
+        FLBR  = FLBR/normalize;
+        FRBL = FRBL/normalize;
+        if (FLBR > 1) {
+            FRBL /= FLBR;
+            FLBR /= FLBR;
+        }
+        if (FRBL > 1) {
+            FLBR /= FRBL;
+            FRBL /= FRBL;
+        }
+        //FUCK floating points
+        if (Math.abs(FLBR) < .0001) {
+            FLBR = 0;
+        }
+        if (Math.abs(FRBL) < .0001) {
+            FRBL = 0;
+        }
+        telemetry.addData("VECTORS: ", String.valueOf(FLBR) + " " + String.valueOf(FRBL) + String.valueOf(FRBL == 1));
+        
+        while(opModeIsActive()) {
+            
+            telemetry.update();
+            break;
+        }
+        frontLeft.setTargetPosition((int)(ticksPerInch * hyp/FLBR));
+        frontRight.setTargetPosition((int)(ticksPerInch * hyp/FRBL));
+        backLeft.setTargetPosition((int)(ticksPerInch * hyp/FRBL));
+        backRight.setTargetPosition((int)(ticksPerInch * hyp/FLBR));
+        
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        
+        frontLeft.setPower(FLBR);
+        frontRight.setPower(FRBL);
+        backLeft.setPower(FRBL);
+        backRight.setPower(FLBR);
+        while (frontLeft.isBusy() || frontRight.isBusy() || backLeft.isBusy() || backRight.isBusy()) {
+            telemetry.addData("Motors", 
+            frontLeft.getCurrentPosition() + " " +
+            frontRight.getCurrentPosition() + " " +
+            backLeft.getCurrentPosition() + " " +
+            backRight.getCurrentPosition());
+            telemetry.update();
+        }
+        //Stop moving and be at new spot
+        xPos = xDest;
+        yPos = yDest;
+    }
 }
+//https://cad.onshape.com/documents/8984e00f04b05696d8db3ecb/w/967eeac02a8de364065c7c08/e/193021613c0f3adca058692b
